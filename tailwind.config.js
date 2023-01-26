d0c75ac87df3936d1c1a4608675d162ee2aecab0
julia-iPorto <julia@iporto.com>
2023-01-26 12:36:07 -0300

    Desenvolvimento do html da home para landing page

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        bebas: ['Bebas Neue, sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+        nova: ["PROXIMA NOVA", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
