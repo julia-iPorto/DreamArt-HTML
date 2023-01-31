3e889de715652ac9acc02edd9190eb9c5fee5236
julia-iPorto <julia@iporto.com>
2023-01-31 16:23:39 -0300

    HTML Dream Art 2.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
     extend: {
+      screens: {
+        'xl': {'min': '1401px'},
+        // => @media (max-width: 1279px) { ... }
+  
+        'lg': {'max': '1400px'},
+        // => @media (max-width: 1023px) { ... }
+  
+        'md': {'max': '767px'},
+        // => @media (max-width: 767px) { ... }
+  
+        'sm': {'max': '639px'},
+        // => @media (max-width: 639px) { ... }
+      },
       fontFamily: {
-        bebas: ['Bebas Neue, sans-serif'],
-        poppins: ['Poppins', 'sans-serif'],
-        nova: ["PROXIMA NOVA", "sans-serif"],
+        barlow: ['"Barlow"', ...defaultTheme.fontFamily.sans],
+        bebas: ['"Bebas Neue"', ...defaultTheme.fontFamily.sans]
       },
     },
   },
